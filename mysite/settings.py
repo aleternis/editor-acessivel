d223e3bef1cc947ea144ebb702ab21cff4c6f60c
tayanemoura <tayanemoura@id.uff.br>
2016-04-24 16:58:57 -0300

    add mysql client

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'enem',
-        'USER': 'alexandre',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tcc',
+        'USER': 'root',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '',
     }
