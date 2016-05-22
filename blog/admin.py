36169b3d020782b26fa50581a05236fedcd04c0f
tayanemoura <tayanemoura@id.uff.br>
2016-05-22 20:38:56 -0300

    add Exam and ExamTemplate admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post, Comment, Question
+from .models import Post, Comment, Question, Exam, ExamTemplate
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Question)
+admin.site.register(Exam)
+admin.site.register(ExamTemplate)
