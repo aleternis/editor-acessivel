fb057329ac0d0fabfb3fde02117d29ad7fdbd835
alexandre <aleternis@hotmail.com>
2016-06-05 20:45:28 -0300

    additional files and changes for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -149,3 +149,17 @@ TINYMCE_DEFAULT_CONFIG = {
     'paste_text_sticky_default' : True,
     'valid_styles' : 'font-weight,font-style,text-decoration',
 }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
