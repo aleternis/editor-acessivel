4d7bd139260a625bc710abd34dfbfe64e774ffee
alexandre <aleternis@hotmail.com>
2016-06-05 23:50:00 -0300

    Retirando mudancas heroku e aducionando bordar em imagem sem alt

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,16 +150,4 @@ TINYMCE_DEFAULT_CONFIG = {
     'valid_styles' : 'font-weight,font-style,text-decoration',
 }
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
