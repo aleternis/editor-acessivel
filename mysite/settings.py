1cfdf57ddc3c7cb0b71a82518ad948ec7ec4e6d2
tayanemoura <tayanemoura@id.uff.br>
2016-07-10 16:11:06 -0300

    more small fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,7 +148,7 @@ TINYMCE_DEFAULT_CONFIG = {
     'paste_text_sticky': True,
     'paste_text_sticky_default' : True,
     'valid_styles' : 'font-weight,font-style,text-decoration',
-    'height' : '700',
+    'height' : '450',
 }
 
 
