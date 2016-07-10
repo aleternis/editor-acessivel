369c3c20589fa28dc88de8aa0e123dba68598483
tayanemoura <tayanemoura@id.uff.br>
2016-07-10 12:36:27 -0300

    small fixes

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Post, Comment, Question, Exam, ExamTemplate, Essay
+from .models import Question, Exam, ExamTemplate, Essay
 
-admin.site.register(Post)
-admin.site.register(Comment)
 admin.site.register(Question)
 admin.site.register(Exam)
 admin.site.register(ExamTemplate)
